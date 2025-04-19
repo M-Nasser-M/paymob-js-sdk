3096c597c0ec2da9fab889cdd8476fc0ef75f125
nasser <m.nasser.15910@gmail.com>
2025-04-19 22:01:04 +0200

    Simplify build config and remove dual ESM/CJS output format

--- src/env.ts
+++ src/env.ts
@@ -1,24 +1,24 @@
 import * as v from "valibot";
 
 const ENVSchema = v.object({
-	MOBILE_WALLET_INTEGERATION_ID: v.pipe(
-		v.string(),
-		v.transform((input) => Number(input)),
-		v.number(),
-	),
-	CARD_INTEGERATION_ID: v.pipe(
-		v.string(),
-		v.transform((input) => Number(input)),
-		v.number(),
-	),
-	PAYMOB_API_KEY: v.string(),
-	PAYMOB_PUBLIC_KEY: v.string(),
-	PAYMOB_SECRET_KEY: v.string(),
-	PAYMOB_NOTIFICATION_URL: v.pipe(v.string(), v.url()),
-	PAYMOB_REDIRECTION_URL: v.pipe(v.string(), v.url()),
+  MOBILE_WALLET_INTEGERATION_ID: v.pipe(
+    v.string(),
+    v.transform((input) => Number(input)),
+    v.number()
+  ),
+  CARD_INTEGERATION_ID: v.pipe(
+    v.string(),
+    v.transform((input) => Number(input)),
+    v.number()
+  ),
+  PAYMOB_API_KEY: v.string(),
+  PAYMOB_PUBLIC_KEY: v.string(),
+  PAYMOB_SECRET_KEY: v.string(),
+  PAYMOB_NOTIFICATION_URL: v.pipe(v.string(), v.url()),
+  PAYMOB_REDIRECTION_URL: v.pipe(v.string(), v.url()),
 });
 
-const env = Bun.env;
+const env = process.env;
 
 const envResult = v.parse(ENVSchema, env);
 
